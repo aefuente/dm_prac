73b4b3f9b13500809c299ecbbeac56f6fd95bb1b
Andre Fuentes <andfnts@gmail.com>
2021-10-17 15:22:16 -0500

    cleaning

--- data.py
+++ data.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 class data(object):
     def __init__(self, train_csv, test_csv, columns):
-        self.train_x = pd.read_csv(train_csv, skipinitialspace=True,skiprows=1)
-        self.test_x = pd.read_csv(test_csv, skipinitialspace=True, skiprows=1)
+        self.train_x = pd.read_csv(train_csv, skipinitialspace=True)
+        self.test_x = pd.read_csv(test_csv, skipinitialspace=True)
         self.train_x.columns = columns
         self.test_x.columns = columns
         self.train_y = None
